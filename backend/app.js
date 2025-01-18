67d87af07c42fdf50c74e88d55861487908de7d3
tanmay <tanmaytalekar13@gmail.com>
2025-01-18 18:25:36 +0530

    fixing script

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,9 @@ app.use((req, res) => {
     res.status(404).json({ message: "Route not found" });
 });
 
-
+app.get("/", (req, res) => {
+    res.send("Welcome to the API!");
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
