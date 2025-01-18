83a01efac59ec50f146628b89ec311a0e28a8a35
tanmay <tanmaytalekar13@gmail.com>
2025-01-18 20:28:57 +0530

    final commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import AdminLogin from './pages/AdminLogin'
 import AdminDashboard from './pages/AdminDashboard'
@@ -7,20 +7,18 @@ import ProtectedRoute from './components/ProtectedRoute'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path='/admin-login' element={<AdminLogin />} />
-        <Route 
-          path='/admin/dashboard' 
-          element={
-            <ProtectedRoute>
-              <AdminDashboard />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path='/admin-login' element={<AdminLogin />} />
+      <Route 
+        path='/admin/dashboard' 
+        element={
+          <ProtectedRoute>
+            <AdminDashboard />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
   )
 }
 
